59ecfee5f5692b20ce1e48614bee1b24ba688da0
mack <mariegilbackup@gmail.com>
2024-10-01 18:33:31 +0200

    sticks n stones
    why the fuck did waggington break

---
+++ Assets/Level/Maps/SticksAndStones/sticks n stones objects.tsx
@@ -0,0 +1,139 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="sticks n stones objects" tilewidth="616" tileheight="326" tilecount="45" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="43" height="45" source="objs/barrel.png"/>
+ </tile>
+ <tile id="1">
+  <image width="110" height="196" source="objs/beads.png"/>
+ </tile>
+ <tile id="2">
+  <image width="116" height="58" source="objs/box wrapping paper.png"/>
+ </tile>
+ <tile id="4">
+  <image width="90" height="88" source="objs/box-with-scrolls.png"/>
+ </tile>
+ <tile id="5">
+  <image width="126" height="66" source="objs/carpet.png"/>
+ </tile>
+ <tile id="6">
+  <image width="92" height="82" source="objs/chest.png"/>
+ </tile>
+ <tile id="8">
+  <image width="71" height="33" source="objs/circle.png"/>
+ </tile>
+ <tile id="9">
+  <image width="35" height="34" source="objs/circles.png"/>
+ </tile>
+ <tile id="10">
+  <image width="35" height="34" source="objs/circles2.png"/>
+ </tile>
+ <tile id="11">
+  <image width="273" height="225" source="objs/counter.png"/>
+ </tile>
+ <tile id="13">
+  <image width="194" height="221" source="objs/display cabinet.png"/>
+ </tile>
+ <tile id="15">
+  <image width="616" height="326" source="objs/display floor.png"/>
+ </tile>
+ <tile id="14">
+  <image width="80" height="180" source="objs/display pillar.png"/>
+ </tile>
+ <tile id="16">
+  <image width="346" height="207" source="objs/display table.png"/>
+ </tile>
+ <tile id="12">
+  <image width="178" height="210" source="objs/display2.png"/>
+ </tile>
+ <tile id="17">
+  <image width="95" height="193" source="objs/fabric.png"/>
+ </tile>
+ <tile id="40">
+  <image width="222" height="227" source="objs/green display 1.png"/>
+ </tile>
+ <tile id="41">
+  <image width="252" height="235" source="objs/green display 2.png"/>
+ </tile>
+ <tile id="42">
+  <image width="240" height="229" source="objs/green display 3.png"/>
+ </tile>
+ <tile id="19">
+  <image width="24" height="46" source="objs/jar red.png"/>
+ </tile>
+ <tile id="21">
+  <image width="17" height="26" source="objs/jar green.png"/>
+ </tile>
+ <tile id="22">
+  <image width="16" height="25" source="objs/jar blue.png"/>
+ </tile>
+ <tile id="23">
+  <image width="17" height="27" source="objs/jar yellow.png"/>
+ </tile>
+ <tile id="24">
+  <image width="31" height="29" source="objs/paint can blue.png"/>
+ </tile>
+ <tile id="29">
+  <image width="113" height="54" source="objs/pawprint dog1.png"/>
+ </tile>
+ <tile id="30">
+  <image width="68" height="27" source="objs/pawprint dog2.png"/>
+ </tile>
+ <tile id="31">
+  <image width="106" height="121" source="objs/pawprint dog3.png"/>
+ </tile>
+ <tile id="32">
+  <image width="44" height="33" source="objs/pawprint dog4.png"/>
+ </tile>
+ <tile id="27">
+  <image width="38" height="29" source="objs/pawprint horse1.png"/>
+ </tile>
+ <tile id="34">
+  <image width="57" height="37" source="objs/pawprint horse2.png"/>
+ </tile>
+ <tile id="35">
+  <image width="93" height="25" source="objs/pawprint horse3.png"/>
+ </tile>
+ <tile id="26">
+  <image width="37" height="24" source="objs/pawprint pig1.png"/>
+ </tile>
+ <tile id="28">
+  <image width="79" height="23" source="objs/pawprint pig2.png"/>
+ </tile>
+ <tile id="49">
+  <image width="97" height="136" source="objs/pencil barrier 1.png"/>
+ </tile>
+ <tile id="18">
+  <image width="130" height="143" source="objs/pencil barrier.png"/>
+ </tile>
+ <tile id="37">
+  <image width="87" height="160" source="objs/potted bush.png"/>
+ </tile>
+ <tile id="36">
+  <image width="29" height="34" source="objs/potted plant.png"/>
+ </tile>
+ <tile id="38">
+  <image width="112" height="103" source="objs/safe.png"/>
+ </tile>
+ <tile id="39">
+  <image width="109" height="45" source="objs/shelf.png"/>
+ </tile>
+ <tile id="44">
+  <image width="113" height="82" source="objs/stack of boxes.png"/>
+ </tile>
+ <tile id="45">
+  <image width="90" height="118" source="objs/star.png"/>
+ </tile>
+ <tile id="47">
+  <image width="64" height="121" source="objs/tall lamp.png"/>
+ </tile>
+ <tile id="48">
+  <image width="75" height="157" source="objs/tree hanging clothes.png"/>
+ </tile>
+ <tile id="46">
+  <image width="123" height="79" source="objs/wood bench.png"/>
+ </tile>
+ <tile id="43">
+  <image width="96" height="144" source="objs/yellow display.png"/>
+ </tile>
+</tileset>
