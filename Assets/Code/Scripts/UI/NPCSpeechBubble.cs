4319cc2158b66e92afebbc83ef598215a9fd50bf
mack <mariegilbackup@gmail.com>
2025-05-27 21:08:07 +0200

    started npc speech bubble stuff

---
+++ Assets/Code/Scripts/UI/NPCSpeechBubble.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class NPCSpeechBubble : MonoBehaviour
+{
+    // TODO:
+    // Disable two of X / Arrow / Menu GameObjects when one of them is visible
+    // Disable button hitbox GameObject when Menu is visible
+    // Change the color of the "speaker" text, relative to who is "speaking" (purple for an NPC, orange for a minigame...)
+    // Change the size of the Menu to account for several options
+    // Change the size of the speech bubble when the dialogue spans more than one line
+}
